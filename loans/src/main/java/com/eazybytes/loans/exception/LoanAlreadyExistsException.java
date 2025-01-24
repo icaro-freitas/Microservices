1b309b0155c57ae8183e2725d7c765fe6819f916
icaro-freitas <icaro_freitas2014@outlook.com>
2025-01-23 21:55:21 -0300

    Config Server initial configuration

--- loans/src/main/java/com/eazybytes/loans/exception/LoanAlreadyExistsException.java
+++ loans/src/main/java/com/eazybytes/loans/exception/LoanAlreadyExistsException.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class LoanAlreadyExistsException extends RuntimeException {
 
-    private static final long serialVersionUID = 1L;
-
-	public LoanAlreadyExistsException(String message){
+    public LoanAlreadyExistsException(String message){
         super(message);
     }
 
