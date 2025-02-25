dbeeaffa9c402ab71d0e378d74e4e7b33fec4333
icaro-freitas <icaro_freitas2014@outlook.com>
2025-02-24 21:04:35 -0300

    Added Circuitbreaker to accounts feign clients

---
+++ accounts/src/main/java/com/eazybytes/accounts/service/client/CardsFallback.java
@@ -0,0 +1,17 @@
+package com.eazybytes.accounts.service.client;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import com.eazybytes.accounts.dto.CardsDto;
+
+@Component
+public class CardsFallback implements CardsFeignClient{
+
+	@Override
+	public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
