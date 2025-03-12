09dd8d6c76ad201c33a6060299411b6e5c1ae9df
icaro-freitas <icaro_freitas2014@outlook.com>
2025-03-12 19:49:56 -0300

    POM.xml files and code updated for OpenTelemetry

--- accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
@@ -28,39 +28,25 @@ import jakarta.validation.constraints.Pattern;
 @RequestMapping(path = "/api", produces = { MediaType.APPLICATION_JSON_VALUE })
 @Validated
 public class CustomerController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
-	
-    private final ICustomersService iCustomersService;
-    
-    public CustomerController(ICustomersService iCustomersService){
-        this.iCustomersService = iCustomersService;
-    }
 
-	@Operation(
-            summary = "Fetch Customer Details REST API",
-            description = "REST API to fetch Customer details based on a mobile number"
-    )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "HTTP Status OK"
-            ),
-            @ApiResponse(
-                    responseCode = "500",
-                    description = "HTTP Status Internal Server Error",
-                    content = @Content(
-                            schema = @Schema(implementation = ErrorResponseDto.class)
-                    )
-            )
-    }
-    )
+	private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
+	private final ICustomersService iCustomersService;
+
+	public CustomerController(ICustomersService iCustomersService) {
+		this.iCustomersService = iCustomersService;
+	}
+
+	@Operation(summary = "Fetch Customer Details REST API", description = "REST API to fetch Customer details based on a mobile number")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+			@ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))) })
 	@GetMapping("/fetchCustomerDetails")
 	public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(
 			@RequestHeader("eazybank-correlation-id") String correlationId,
 			@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber) {
-		logger.debug("eazyBank-correlation-id found: {} ", correlationId);
+		logger.debug("fetchCustomerDetails method start");
 		CustomerDetailsDto customerDetailsDto = iCustomersService.fetchCustomerDetails(mobileNumber, correlationId);
+		logger.debug("fetchCustomerDetails method end");
 		return ResponseEntity.status(org.apache.http.HttpStatus.SC_OK).body(customerDetailsDto);
 	}
 
